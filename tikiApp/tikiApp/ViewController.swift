3b081bfec096cf90e428d9dfb5d91903692b2669
杨振跃 <yangzhenyue@bigo.sg>
2021-11-06 17:02:51 +0800

    init project

---
+++ tikiApp/tikiApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tikiApp
+//
+//  Created by 杨振跃 on 2021/11/6.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+     override func viewDidLoad() {
+          super.viewDidLoad()
+          // Do any additional setup after loading the view.
+     }
+
+
+}
+
